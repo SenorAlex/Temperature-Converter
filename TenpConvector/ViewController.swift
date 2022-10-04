6043bbace48cd29230017b0e1f5328174e5b090e
Alex Kudanov <sanyasanya312@gmail.com>
2022-10-04 16:31:19 +0300

    Add code

--- TenpConvector/ViewController.swift
+++ TenpConvector/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var celiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
     }
-
-
+    
+    @IBAction func sliderChange(_ sender: UISlider) {
+        let temperatureCelsius = Int(round(sender.value))
+        celiusLabel.text = "\(temperatureCelsius)ºC"
+        let fahrenheitTemperature = Int(round((sender.value  * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(fahrenheitTemperature)ºF"
+    }
 }
 
